c1facc9428a618f50acd8b1cca01d84c4778cdd8
Zack Loveless <zack.loveless@live.com>
2014-07-02 00:37:30 -0400

    first tf2 commit

---
+++ Extensions/TF2Bot.Core/TF2Bot.cs
@@ -0,0 +1,52 @@
+﻿// -----------------------------------------------------------------------------
+//  <copyright file="TF2Bot.cs" company="CnCFPS">
+//      Copyright (c) CnCFPS.  All rights reserved.
+//  </copyright>
+// -----------------------------------------------------------------------------
+
+namespace TF2Bot.Core
+{
+    using Atlantis.Net.Irc;
+    using Lantea.Common.Extensibility;
+    using Lantea.Common.IO;
+
+    // ReSharper disable InconsistentNaming
+
+    public class TF2Bot : IModule
+    {
+        #region Implementation of IModule
+
+        public string Author
+        {
+            get { return "Zack Loveless"; }
+        }
+
+        public string Description
+        {
+            get { return "A TF2 server regulator."; }
+        }
+
+        public string Name
+        {
+            get { return "TF2Bot"; }
+        }
+
+        public string Version
+        {
+            get { return "1.0"; }
+        }
+
+        public void Dispose()
+        {
+        }
+
+        public void Initialize(Block config, IrcClient client)
+        {
+            // 
+        }
+
+        #endregion
+    }
+
+    // ReSharper restore InconsistentNaming
+}
